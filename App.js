e4803efc7217f84c3b4fe16801266482bd43f893
Rihan Mohammed <rihanmohammed26@gmail.com>
2022-07-04 23:24:00 +0530

    final commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import 'react-native-gesture-handler';
+import React from 'react';
+import AppNavContainer from './src/navigations';
+import GlobalProvider from './src/context/Provider';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+	return (
+		<GlobalProvider>
+			<AppNavContainer />
+		</GlobalProvider>
+	);
+};
+
+export default App;
